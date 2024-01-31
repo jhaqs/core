61d7313e34987715388eed695636eeff5206840e
jAnderson <jhon.arones@inetum.com>
2024-01-31 10:53:42 -0500

    se agrega el proyecto inicial

---
+++ src/main/java/edu/tienda/core/MsSpringBootCoreDeTiendaOnlineApplication.java
@@ -0,0 +1,13 @@
+package edu.tienda.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsSpringBootCoreDeTiendaOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsSpringBootCoreDeTiendaOnlineApplication.class, args);
+	}
+
+}
---
+++ src/main/java/edu/tienda/core/controllers/HolaMundoController.java
@@ -0,0 +1,11 @@
+package edu.tienda.core.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HolaMundoController {
+
+    public String saludo(){
+        return "Hola mundo Spring boot";
+    }
+}
